e1b4d37b906dcd943570860ea684c9d09a3cdf19
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-07-13 20:19:18 +0530

    Made background color selector to change background color by clearing grid, add a range slider to change grid size.

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 const container = document.getElementById("container");
 const fgColorInput = document.getElementById("fgColorInput");
 const bgColorInput = document.getElementById("bgColorInput");
+const randomColorToggle = document.getElementById("randomColorToggle");
+const gridSlider = document.getElementById("gridSlider");
+const sliderLabel = document.getElementById("sliderLabel");
 
-const defaultGridSize = 32;
+var gridSize = 24;
 
-var gridSize = defaultGridSize;
+sliderLabel.textContent = "24";
+
+gridSlider.addEventListener("change", function () {
+  gridSize = this.value;
+  sliderLabel.textContent = gridSize;
+  clearGrid();
+});
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
-container.style.cssText = `
-grid-template-columns: repeat(${gridSize}, 1fr);
-grid-template-rows: repeat(${gridSize}, 1fr);`;
+var bgColor = "#ffffff";
+console.log(bgColor)
+bgColorInput.addEventListener("input", (event) => {
+  bgColor = event.target.value;
+  clearGrid();
+});
 
 createGrid(gridSize);
 
-function createGrid(gridSize) {
-  for (let i = 0; i < gridSize ** 2; i++) {
+function createGrid(size) {
+  container.style.cssText = `
+  grid-template-columns: repeat(${gridSize}, 1fr); 
+  grid-template-rows: repeat(${gridSize}, 1fr);`;
+  for (let i = 0; i < size ** 2; i++) {
     const gridBox = document.createElement("div");
+    gridBox.style.backgroundColor = bgColor;
     gridBox.className = "containerItem";
-    gridBox.style.height = `calc(75vh/${gridSize})`;
+    gridBox.style.height = `calc(75vh/${size})`;
     gridBox.addEventListener("mousedown", changeColor);
     gridBox.addEventListener("mouseover", changeColor);
     container.appendChild(gridBox);
@@ -38,13 +54,28 @@ function createGrid(gridSize) {
   }
 }
 
-var color = "#000";
-fgColorInput.addEventListener("input", function (event) {
-  color = event.target.value;
+var randomizeColor = false;
+randomColorToggle.addEventListener("click", function () {
+  randomColorToggle.classList.toggle("randomColorActive");
+  if (randomizeColor == false) randomizeColor = true;
+  else randomizeColor = false;
+});
+
+function randomColor() {
+  var color = `#${Math.floor(Math.random() * 16581376).toString(16)}`;
+  return color;
+}
+
+var stableColor = "#000000";
+fgColorInput.addEventListener("input", (event) => {
+  stableColor = event.target.value;
 });
 
 const fgColor = () => {
-  return color;
+  var returnColor = "";
+  if (randomizeColor) returnColor = randomColor();
+  else returnColor = stableColor;
+  return returnColor;
 };
 
 function changeColor(e) {
